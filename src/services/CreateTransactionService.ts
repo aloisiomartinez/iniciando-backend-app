b1d439c88b917d31323c37f0cc57e4fd546b6711
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-05-07 20:30:00 -0300

    Transaction Migration

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
 // import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+
+    /**
+    const checkCategoryExists = await transactionsRepository.findOne9{
+      where: {category}
+    }
+    const (checkCategoryExists) {
+     //Usar ID existente da categoria ja existente
+    }
+    */
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
